35543fe8e5327977c8444ec0248c5a2a1fb3d768
Edison Lam <elam3@mail.ccsf.edu>
2016-11-28 08:50:48 -0800

    race condition

--- poll_app/polls/views.py
+++ poll_app/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.db.models import F
 
 from .models import Question, Choice
 
@@ -25,7 +26,8 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(choice_text=request.POST['choice'])
+        selected_choice_set = question.choice_set.all().filter(choice_text=request.POST['choice'])
+        #selected_choice = question.choice_set.get(choice_text=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Re-display the question voting form.
         return render(request, 'polls/detail.html', {
@@ -33,8 +35,9 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        #selected_choice.votes += 1
+        #selected_choice.save()
+        selected_choice_set.update(votes=F('votes')+1)
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the back button.
